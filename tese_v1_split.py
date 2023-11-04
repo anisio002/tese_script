b02da6174a02c57340ebacf1a59024ad351cdc59
Anísio Silva Jr <35922946+anisio002@users.noreply.github.com>
2023-11-04 11:09:08 -0300

    Update tese_v1_split.py
    Versão Final

--- tese_v1_split.py
+++ tese_v1_split.py
@@ -1,3 +1,5 @@
+from google.colab import drive
+drive.mount('/content/drive')
 import pandas as pd
 import tensorflow as tf
 from tensorflow.keras.callbacks import EarlyStopping
@@ -12,16 +14,18 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.svm import SVR
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
+import numpy as np
 import csv
 import time
 from math import sqrt
+import sys
+sys.path.insert(0, "/content/drive/MyDrive/Colab Notebooks")
 from TeseFunctios import TeseFunctios as tese
 
-save_path = "/home/anisio/Documentos/Doutorado/Tese/Dados/tese/models/"
-path = "/home/anisio/Documentos/Doutorado/Tese/Dados/Pareados"
-file_path = "/home/anisio/Documentos/Doutorado/Tese/Dados/tese/rede/"
-#Dataset utilizado para validação
-dadosParaiso = pd.read_csv(path + '/GO-ALTO PARAISO DE GOIAS.csv',sep=',')
+save_path = "/content/drive/MyDrive/Doutorado/Dados/tese/PINN/"
+path = "/content/drive/MyDrive/Doutorado/Dados/Pareados"
+file_path = "/content/drive/MyDrive/Doutorado/Dados/tese/PINN/"
+
 
 dadosCampinaVerde = pd.read_csv(path + '/MG-CAMPINA VERDE.csv',sep=',')
 dadosSorriso = pd.read_csv(path + '/MT-SORRISO.csv',sep=',')
@@ -34,7 +38,7 @@ dadosIrece = pd.read_csv(path + '/BA-IRÊCE.csv',sep=',')
 
 listNames = ["Campo Bom - RS","Sorriso - MT","Diamante do Norte - PR","Campina Verde - MG","Barcelos - AM", "Macapá - AP", "Petrolina - PE", "Irêce - BA"]
 listaDados = [dadosCampo,dadosSorriso,dadosDiamante,dadosCampinaVerde,dadosBarcelos,dadosMacapa,dadosPetrolina,dadosIrece]
-data = pd.read_csv(path + '/AP-MACAPÁ.csv',sep=',')
+
 
 fields = ['City', 'TrainningSize','RMSE','Time', 'Epoch','Training_Loss', 'Training_Accuracy', 'Test_Loss', 'Test_Accuracy', 'RMSE_UNKNOW']
 functions = ['f1','rmse']
@@ -42,29 +46,17 @@ training_sets = [0.2,0.4,0.6]
 
 pos = ['umid_inst', 'pto_orvalho_inst', 'pressao', 'radiacao', 'vento_vel']
 
-y_validation = dadosParaiso['temp_inst'].to_numpy()
-X_validation= dadosParaiso.drop(['temp_inst'],axis = 1)
-scaler = StandardScaler().fit(X_validation)
-X_validation = scaler.transform(X_validation)
-
-epochs = 1
+epochs = 3000
 seed = 7
 learning_rate = 1e-4
 min_delta = 1e-4
-patience = 1
+patience = 10
 
 pinn = []
 dl = []
 rmse = []
 rmse_dl = []
 mlp_time=[]
-
-class HeatEquationLoss(tf.keras.losses.Loss):
-    def __init__(self, name='heat_equation_loss', **kwargs):
-        super(HeatEquationLoss, self).__init__(name=name, **kwargs)
-
-    def call(self, y_true, y_pred):
-        return tese.heat_equation_loss(y_true, y_pred)
 
 class MyCallback(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs=None):
@@ -74,16 +66,16 @@ class MyCallback(tf.keras.callbacks.Callback):
 callback = MyCallback()
 early_stopping = EarlyStopping(monitor= 'root_mean_squared_error', min_delta=min_delta, patience=patience)
 
-    
 contCity =0
 for data in listaDados:
     city = listNames[contCity]
-    
+
     y = data['temp_inst'].to_numpy()
     X = data.drop(['temp_inst'],axis = 1)
+    X_o = data.drop(['temp_inst'],axis = 1).values
     scaler = StandardScaler().fit(X)
     X = scaler.transform(X)
-    
+
     pipelines = []
     pipelines.append(('LR', Pipeline([('Scaler', StandardScaler()),('LR',LinearRegression())])))
     pipelines.append(('LASSO', Pipeline([('Scaler', StandardScaler()),('LASSO',Lasso())])))
@@ -93,34 +85,60 @@ for data in listaDados:
     pipelines.append(('SVR', Pipeline([('Scaler', StandardScaler()),('SVR', SVR())])))
     pipelines.append(('PINN', Pipeline([('Scaler', StandardScaler()),('PINN', None)])))
     pipelines.append(('MLP', Pipeline([('Scaler', StandardScaler()),('MLP', None)])))
-    
+
     for name, model in pipelines:
-        file_name = file_path + 'Split_' + name + '_' + city
-        with open(file_name + '.csv', 'a', newline='') as file:
+        with open(file_path +'Split_'+ name + '_T_' + city + '.csv', 'a', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(fields)
-            
+
     for test_size in training_sets:
-    
+
         for name, model in pipelines:
+            file_name = file_path +'Split_'+ name + '_T_' + city
             results=[]
             X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=seed)
-                
+            X_f_train, X_f_test, y_f_train, y_f_test= train_test_split(X_o, y, test_size=test_size, random_state=seed)
+
+            cont_batch = 0
+            def custom_cost_function(y_true,y_pred):
+                    # Parâmetros
+                    R = np.mean(X_f_train[:,2]) *1000 #radiação em kj/m²
+                    rho = 1.184
+                    ca= 1012
+                    sigma = 5.67e-8
+                    T_pred= tf.squeeze(y_pred) + 273.15 #temperatura em celsius
+                    T_true= tf.squeeze(y_true) + 273.15 #temperatura em celsius
+                    alpha = 2.7e-3
+                    gamma = 6.5e-4
+                    d2T_dt2_pred = (alpha * R - gamma * sigma * T_pred**4) / (ca * rho)
+                    d2T_dt2_true = (alpha * R - gamma * sigma * T_true**4) / (ca * rho)
+
+                    correction_factor = tf.abs(d2T_dt2_true) - tf.abs(d2T_dt2_pred)
+                    loss =tf.reduce_mean( tf.square(y_true - y_pred ) + d2T_dt2_pred)
+                    return loss
+
+            class HeatEquationLoss(tf.keras.losses.Loss):
+              def __init__(self, name='heat_equation_loss', **kwargs):
+                  super(HeatEquationLoss, self).__init__(name=name, **kwargs)
+
+              def call(self, y_true, y_pred):
+                  return custom_cost_function(y_true, y_pred)
+
             history= None
             num_epochs = None
             y_pred = None
             train_score = None
             test_score = None
             y_unknow = None
-            
+
             inicio = time.time()
             if name not in ["MLP", "PINN"]:
                 print(">>>>"+name)
                 model.fit(X_train, y_train)
-                train_score = [None, model.score(X_train, y_train)] 
-                test_score = [None, model.score(X_test, y_test),None ] 
+                train_score = [None, model.score(X_train, y_train)]
+                test_score = [None, model.score(X_test, y_test),None ]
                 y_pred = model.predict(X_test)
-                y_unknow = model.predict(X_validation)
+
             else:
                 model_rn = None
                 if name == "MLP":
@@ -135,17 +153,19 @@ for data in listaDados:
                 train_score = model_rn.evaluate(X_train, y_train)  # Avalie o modelo
                 test_score = model_rn.evaluate(X_test, y_test)
                 y_pred = model_rn.predict(X_test)
-                y_unknow = model_rn.predict(X_validation)
                 num_epochs = len(history.epoch)
                 tese.plot_rmse(history, None,file_name,city,name,test_size)
             fim = time.time()
-                
+
+            dataY = {'y_test': y_test, 'y_pred': y_pred.ravel()}
+            dfY = pd.DataFrame(dataY)
+            dfY.to_csv(file_name + f'{str(test_size)}_Y_.csv', index=False)
             mlp_time = fim - inicio
             print(f'Testsize: {str(test_size)} - Localidade: {city}')
-            results.append([city, str(test_size), sqrt(mean_squared_error(y_test, y_pred)), mlp_time, num_epochs, train_score[0],train_score[1],test_score[0],test_score[1],sqrt(mean_squared_error(y_validation, y_unknow))])
+            results.append([city, str(test_size), sqrt(mean_squared_error(y_test, y_pred)), mlp_time, num_epochs, train_score[0],train_score[1],test_score[0],test_score[1]])
             values = ','.join(str(v) for v in results)
             with open(file_name + '.csv', 'a', newline='') as file:
                 writer = csv.writer(file)
                 writer.writerows(results)
                 print("List saved to CSV file successfully.")
-    contCity += 1            
+    contCity += 1
